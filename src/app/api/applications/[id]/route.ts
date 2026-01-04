0b6639febfdf145229834d2e76375b8f77506957
suwuk <diaz.yudha29@gmail.com>
2026-01-04 17:12:12 +0700

    fix error auth

--- src/app/api/applications/[id]/route.ts
+++ src/app/api/applications/[id]/route.ts
@@ -1,59 +1,7 @@
-import { NextResponse } from "next/server";
-import { deleteApplication, patchApplication } from "@/lib/firebase/service";
-import { revalidateTag } from "next/cache";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-type RouteContext = {
-  params: Promise<{ id: string }>;
-};
+const handler = NextAuth(authOptions);
 
-export async function DELETE(request: Request, { params }) {
-  const { id } = await params;
-  try {
-    await deleteApplication(id);
-    revalidateTag("applications");
-    return NextResponse.json(
-      { message: "Data deleted successfully" },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to delete data" },
-      { status: 500 }
-    );
-  }
-}
-
-export async function PATCH(
-  req: Request,
-  context: RouteContext 
-) {
-  try {
-    const body = await req.json();
-    const { id } = await context.params;
-
-    if (!id) {
-      return NextResponse.json(
-        { message: "ID lamaran tidak ditemukan" },
-        { status: 400 }
-      );
-    }
-
-    await patchApplication(id, body);
-
-    return NextResponse.json(
-      {
-        status: true,
-        message: "Lamaran berhasil diperbarui",
-      },
-      { status: 200 }
-    );
-  } catch (error: any) {
-    return NextResponse.json(
-      {
-        status: false,
-        message: error.message || "Terjadi kesalahan pada server",
-      },
-      { status: 500 }
-    );
-  }
-}
+// Hanya export GET dan POST
+export { handler as GET, handler as POST };
